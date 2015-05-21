6aa05452a3b271604cf8dea5554d28c9ea1aa5b4
Drogas <YOVANI_719@hotmail.com>
2015-05-21 17:56:27 -0500

    calendario
    promos con calendario

--- login/promotion.php
+++ login/promotion.php
@@ -1,8 +1,7 @@
 <?php  		
-	//connection
+	//con:nection
 	include("functions.php");
-	$conex = connection();
-	
+	$conex = connection();	
 	//session start
 	session_start();
 ?>
@@ -14,151 +13,88 @@
 			<h4 class="promotion_title">Promotions</h4>
 			<!--categories botons-->
 			<div class="btn-group btn-group-justified promotion_botons" role="group" aria-label="...">
+			<?php
+				$category = "SELECT id, name FROM category";
+				$category_result = $conex->query($category);
+				while ($category_row = $category_result->fetch_array(MYSQLI_ASSOC)){
+				  $category_row_name = $category_row['name'];
+				  $category_row_id = $category_row['id'];
+			?>				
 			  <div class="btn-group" role="group">
-				<button type="button" class="btn btn-primary">Bar</button>
+			    <a class="btn btn-primary cat_places" id_category="<?php echo $category_row_id;?>" href="#" role="button"><?php echo $category_row_name;?></a>
 			  </div>
-			  <div class="btn-group" role="group">
-				<button type="button" class="btn btn-primary">Restaurant</button>
-			  </div>
-			  <div class="btn-group" role="group">
-				<button type="button" class="btn btn-primary">Workoffice</button>
-			  </div>
-			  <div class="btn-group" role="group">
-				<button type="button" class="btn btn-primary">Zoo</button>
-			  </div>
+			<?php	
+				}
+			?>
 			</div>
 			</br>
 			<!--carrucel-->					
-			<div id="owl-demo" class="owl-carousel owl-theme">			
-				<?php
-					//select the table promotions
-					$select_promotion = "SELECT * FROM promotion";
-					$result = $conex->query($select_promotion);
-					while ($row = $result->fetch_array(MYSQLI_ASSOC)){
-					//$row = $result->fetch_array(MYSQLI_ASSOC);
-					  $day = $row['day'];
-					  $promotion = $row['promotion'];
-					  $image = $row['image'];
-					  $place = $row['place_id'];
-					  $category = $row['category_id'];
-					  //place
-					  $select_place = "SELECT name, address, city, phone, schedule, rating, tags_id FROM place WHERE id='$place'";
-					  $result_place = $conex->query($select_place);
-					  $row_place = $result_place->fetch_array(MYSQLI_ASSOC);
-						$place_name = $row_place['name'];
-						$place_address = $row_place['address'];
-						$place_city = $row_place['city'];
-						$place_phone = $row_place['phone'];
-						$place_schedule = $row_place['schedule'];
-						$place_rating = $row_place['rating'];
-						$place_tags = $row_place['tags_id'];
-					  //category	
-					  $select_category = "SELECT name FROM category WHERE id='$category'";
-					  $result_category = $conex->query($select_category);
-					  $row_category = $result_category->fetch_array(MYSQLI_ASSOC);
-						$category_name = $row_category['name'];	
-					  //image
-					  $select_image = "SELECT * FROM image WHERE id='$image'";
-					  $result_image = $conex->query($select_image);
-					  $row_image = $result_image->fetch_array(MYSQLI_ASSOC);
-						$ext = $row_image['img_type'];
-						$image_type = $row_image['type'];	
-						$filename = "../photos/$image_type/$image$ext";
-									
-						if (file_exists($filename)) {
-							$filename = $filename;
-						} 
-						else {
-							$filename = "../photos/$image_type/default.png";
-						}
-					  //tags	
-					  $select_tags = "SELECT name FROM tags WHERE id='$place_tags'";
-					  $result_tags = $conex->query($select_tags);
-					  $row_tags = $result_tags->fetch_array(MYSQLI_ASSOC);
-						$tags_name = $row_tags['name'];		
-				?>		
-			  <!--pages of carrusel-->
-			  <div class="item">
-			    <div class="col-md-3 promotion_info_place">
-				  <h4 class="promotion_title_cat_place">Place information</h4> </br>
-			  	  <p class="promotion_content_place">				  	
-					  <?php echo $place_address;?></br>
-					  <?php echo $place_city;?></br>
-					  <?php echo $place_phone;?></br>
-					  <?php echo $place_schedule;?></br>
-				  </p>
-				</div>
-				<div class="col-md-6">
-				  <h4 class="promotion_title_name_place"><?php echo $place_name;?></h4></br>
-				  <h5 class='promotion_place_raiting'>
-				  	  <?php
-					  	$cont = 0;
-					  	for($cont; $cont<$place_rating; $cont ++){
-							if($cont==0){
-								echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
-							}
-							if($cont==1){
-								echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
-							}
-							if($cont==2){
-								echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
-							}
-							if($cont==3){
-								echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
-							}
-							if($cont==4){
-								echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
-							}
-						}
-						for($cont; $cont<5; $cont ++){
-							echo"<span class='glyphicon glyphicon-star' aria-hidden='true'></span>";
-						}						
-					  ?>
-					</h5> </br>
-				    <img class="promotion_image" src="<?php echo $filename; ?>" title="<?php echo $promotion;?>">	
-				</div>
-				<div class="col-md-3 promotion_info_promo">  
-				  <h4 class="promotion_title_cat_place">Promotion</h4></br>
-				  <p class="promotion_content_promo">				  	  
-					  <?php echo $promotion;?></br>
-				  </p>
-				  <h6 class="promotion_line"></h6>
-				  </br>
-				  <h4 class="promotion_title_cat_place">Tags</h4></br>
-				  <p class="promotion_content_tags">				  	  
-					  <?php echo $place_tags;?></br>
-				  </p></br>
-				</div>  
-			  </div>
-			  <?php 
-			  	  }
-				  $conex->close();
-			  ?>
+			<div id="test_cat_pro">			
+				<script>
+					var caldate = $("#datepicker").attr('caldate');
+					$("#test_cat_pro").load("promotion_queries.php?caldate="+caldate);		  	
+				</script>
 			</div>
-			<!--end carrucel-->			
+			<!--end carrucel-->	
+			<!--calendar-->
+			<p class="promotion_calendar">Select date: <input class="promo_calendar" caldate="-1" type="text" id="datepicker" disabled></p>		
 		</div>
 		<div class="col-md-1"></div>
 	</div>
 </div>
 
-
-<!--llamamos el carrusel de las promociones -->
 <script>
-    $(document).ready(function() {
-	/*$("#owl-demo").owlCarousel();*/
-	  var owl = $("#owl-demo");	 
-	  owl.owlCarousel({		 
-		  itemsCustom : [
-			[0, 1],
-			[450, 1],
-			[600, 1],
-			[700, 1],
-			[1000, 1],
-			[1200, 1],
-			[1400, 1],
-			[1600, 1]
-		  ],
-		  navigation : true	 
-	  });	  
+	$(document).ready(function(){		
+		//calendar	
+		var n;	
+		var caldate = $("#datepicker").attr('caldate');
+		var day;
+		var month;
+		$(function() {
+		  $( "#datepicker" ).datepicker({
+		  	    showOn: "button",
+				buttonImage: "../images/calendar.png",
+				buttonImageOnly: true,
+				showAnim: "drop",
+				dateFormat:"dd MM yy",
+				minDate: '-0d', maxDate: '+4m',
+				onSelect: function(dateText){
+					var seldate = $(this).datepicker('getDate');
+					var n = seldate.getUTCDay() + 1;
+					day  = seldate.getDate();
+					month = $.datepicker.formatDate('MM', seldate);
+					//alert(n+", "+day+", "+month);					
+					caldate = n;
+					$("#test_cat_pro").load("promotion_queries.php?caldate="+caldate+"&day="+day+"&month="+month);
+				}
+		  });		 		
+		});
+		
+		//We call the carousel of promotions by category filter
+		$('.cat_places').click(function(){
+			var id_cat = $(this).attr('id_category');
+			$("#test_cat_pro").load("promotion_queries.php?id="+id_cat+"&caldate="+caldate+"&day="+day+"&month="+month);
+		});	
+				
 	});
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ login/promotion_queries.php
@@ -0,0 +1,222 @@
+<?php
+	//connection
+	error_reporting(5);
+	include("functions.php");
+	$conex = connection();
+	
+?>
+	<div id="owl-demo" class="owl-carousel owl-theme">	
+<?php	
+	//check if date is of today of caendar
+	$caldate = $_GET["caldate"];
+	$d;
+	$m;	
+	$num_day;
+	if($caldate == -1){
+		$today = getdate();
+		$d = $today[mday];
+		$m = $today[month];
+		$num_day = $today[weekday];
+		if ($num_day == "Sunday"){
+	  		$num_day=1;
+	    }
+	    if ($num_day == "Monday"){
+	  		$num_day=2;
+	    }
+	    if ($num_day == "Tuesday"){
+	  		$num_day=3;
+	    }
+	    if ($num_day == "Wednesday"){
+	  		$num_day=4;
+	    }
+	    if ($num_day == "Thursday"){
+	  		$num_day=5;
+	    }
+	    if ($num_day == "Friday"){
+	  		$num_day=6;
+	    }
+	    if ($num_day == "Saturday"){
+	  		$num_day=7;
+	    }	
+	}
+	else{
+		$num_day=$caldate;
+		$d = $_GET["day"];	
+		$m = $_GET["month"];
+	}
+	//select * promos  
+	$select_promotion;	
+	if(!$_GET["id"]){
+		$select_promotion = "SELECT * FROM promotion WHERE day = '$num_day' limit 10";
+	}
+	else{
+		$id=$_GET["id"];
+		$select_promotion = "SELECT * FROM promotion WHERE category_id = '$id' and day = '$num_day' limit 10";
+	}
+	//select the table promotions
+	$result = $conex->query($select_promotion);
+	while ($row = $result->fetch_array(MYSQLI_ASSOC)){
+	//$row = $result->fetch_array(MYSQLI_ASSOC);
+	  $day = $row['day'];
+	  $promotion = $row['promotion'];
+	  $image = $row['image'];
+	  $place = $row['place_id'];
+	  $category = $row['category_id'];
+	  //name of day	  
+	  $day_name;
+	  if ($day == 1){
+	  	$day_name="Sunday";
+	  }
+	  if ($day == 2){
+	  	$day_name="Monday";
+	  }
+	  if ($day == 3){
+	  	$day_name="Tuesday";
+	  }
+	  if ($day == 4){
+	  	$day_name="Wednesday";
+	  }
+	  if ($day == 5){
+	  	$day_name="Thursday";
+	  }
+	  if ($day == 6){
+	  	$day_name="Friday";
+	  }
+	  if ($day == 7){
+	  	$day_name="Saturday";
+	  }	  
+	  //place
+	  $select_place = "SELECT id, name, address, city, phone, schedule, rating, tags_id FROM place WHERE id='$place'";
+	  $result_place = $conex->query($select_place);
+	  $row_place = $result_place->fetch_array(MYSQLI_ASSOC);
+	  	$place_id = $row_place['id'];
+		$place_name = $row_place['name'];
+		$place_address = $row_place['address'];
+		$place_city = $row_place['city'];
+		$place_phone = $row_place['phone'];
+		$place_schedule = $row_place['schedule'];
+		$place_rating = $row_place['rating'];
+		$place_tags = $row_place['tags_id'];
+	  //category	
+	  $select_category = "SELECT name FROM category WHERE id='$category'";
+	  $result_category = $conex->query($select_category);
+	  $row_category = $result_category->fetch_array(MYSQLI_ASSOC);
+		$category_name = $row_category['name'];	
+	  //image
+	  $select_image = "SELECT * FROM image WHERE id='$image'";
+	  $result_image = $conex->query($select_image);
+	  $row_image = $result_image->fetch_array(MYSQLI_ASSOC);
+		$ext = $row_image['img_type'];
+		$image_type = $row_image['type'];	
+		$filename = "../photos/$image_type/$image$ext";
+					
+		if (file_exists($filename)) {
+			$filename = $filename;
+		} 
+		else {
+			$filename = "../photos/$image_type/default.png";
+		}			  	  		
+	?>		
+	<!--pages of carrusel-->
+	<div class="item">
+	<div class="col-md-3 promotion_info_place">
+	<h4 class="promotion_title_cat_place">Place information</h4> </br>
+	<p class="promotion_content_place">				  	
+	  <?php echo $place_address;?></br>
+	  <?php echo $place_city;?></br>
+	  <?php echo $place_phone;?></br>
+	  <?php echo $place_schedule;?></br>
+	</p>
+	</div>
+	<div class="col-md-6">
+	<a href="#" onClick="calcRoute(<?php echo $place_id;?>)"><h4 class="promotion_title_name_place" title="Take me here"><?php echo $category_name." ".$place_name;?></h4></a></br>
+	<h5 class='promotion_place_raiting'>
+	  <?php
+		$cont = 0;
+		for($cont; $cont<$place_rating; $cont ++){
+			if($cont==0){
+				echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
+			}
+			if($cont==1){
+				echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
+			}
+			if($cont==2){
+				echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
+			}
+			if($cont==3){
+				echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
+			}
+			if($cont==4){
+				echo"<span class='glyphicon glyphicon-star place_rating_color' aria-hidden='true'></span>";
+			}
+		}
+		for($cont; $cont<5; $cont ++){
+			echo"<span class='glyphicon glyphicon-star' aria-hidden='true'></span>";
+		}						
+	  ?>
+	</h5> </br>
+	<img class="promotion_image" src="<?php echo $filename; ?>" title="<?php echo $promotion;?>">	
+	</div>
+	<div class="col-md-3 promotion_info_promo">  
+	<h4 class="promotion_title_cat_place">Promotion</h4></br>
+	<p class="promotion_content_promo">	
+	  <samp class="promotion_day"><?php echo $day_name." ".$d.", ".$m.".";?></samp></br>			  	  
+	  <?php echo $promotion;?></br>
+	</p>
+	<h6 class="promotion_line"></h6>
+	</br>
+	<h4 class="promotion_title_cat_place">Tags</h4></br>
+	<p class="promotion_content_tags">				  	  
+	  <?php
+		  //tags
+		  $place_tags;	
+		  $tag_list = explode(",", $place_tags);
+		  $tag_ids = array();					  
+		  foreach($tag_list as $tag){
+			  $tag_ids[] = trim($tag, "|");
+		  }		  					  
+		  $select_tags = "SELECT name FROM tags WHERE id in (".implode(",", $tag_ids).") or category_id = 0 limit 10";
+		  $result_tags = $conex->query($select_tags);	
+	   
+		  $cont = 0;
+		  while ($row_tags = $result_tags->fetch_array(MYSQLI_ASSOC)){
+			$cont ++;
+			if ($cont == 10){
+				$tags_name = $row_tags['name'];
+				echo $tags_name.".";
+			}
+			else{
+				$tags_name = $row_tags['name'];
+				echo $tags_name.", ";
+			}						    
+		  }
+	  ?>	
+	</p></br>
+	</div>  
+	</div>
+	<?php 
+	}
+	$conex->close();
+?>
+</div>
+
+<script>
+	var owl = $("#owl-demo");	 
+	owl.owlCarousel({		 
+	  itemsCustom : [
+		[0, 1],
+		[450, 1],
+		[600, 1],
+		[700, 1],
+		[1000, 1],
+		[1200, 1],
+		[1400, 1],
+		[1600, 1]
+	  ],
+	  navigation : true	 
+	});			
+</script>
+
+
+
+
